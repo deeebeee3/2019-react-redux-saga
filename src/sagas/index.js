c71db48c5de53eb549988be6a7ec148cd619c3d1
Deepak Bhari <deepakbhari@deepaks-mbp.lan>
2019-12-13 13:41:50 +0000

    redux saga flow basics

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,16 @@
+import { takeEvery, put } from 'redux-saga/effects';
+
+//worker saga
+function* workerSaga() {
+  console.log('Hey from worker');
+  yield put({ type: 'ACTION_FROM_WORKER' });
+}
+
+//watcher saga
 function* rootSaga() {
-  console.log('Hey World');
+  yield takeEvery('HELLO', workerSaga)
 };
+
+//watcher saga -> listens for an action -> will then invoke worker saga
 
 export default rootSaga;
